3b64ab3e066a46620f0e67ebb946ea3bf0d6668a
Constantine Tarasenkov <iamvfx@gmail.com>
2021-10-28 19:19:19 +0300

    Add more procedures

--- redgpu_f.h
+++ redgpu_f.h
@@ -500,7 +500,7 @@ REDGPU_F_DECLSPEC void                redFManipulatorDisable                  (R
 
 REDGPU_F_DECLSPEC RedFHandleAssimp *  redFCreateAssimp                        (uint64_t count);
 REDGPU_F_DECLSPEC void                redFDestroyAssimp                       (RedFHandleAssimp * handles);
-REDGPU_F_DECLSPEC void                redFAssimpLoadModel                     (RedFHandleAssimp handle, const char * fileName, RedFBool32 optimize);
+REDGPU_F_DECLSPEC RedFBool32          redFAssimpLoadModel                     (RedFHandleAssimp handle, const char * fileName, RedFBool32 optimize);
 REDGPU_F_DECLSPEC unsigned            redFAssimpGetNumMeshes                  (RedFHandleAssimp handle);
 REDGPU_F_DECLSPEC void                redFAssimpGetMeshName                   (RedFHandleAssimp handle, unsigned index, uint64_t * outCharsCountIncludingNullTerminator, char * outChars);
 REDGPU_F_DECLSPEC void                redFAssimpGetMesh                       (RedFHandleAssimp handle, unsigned index, RedFHandleMesh replaceMesh);
@@ -606,6 +606,7 @@ REDGPU_F_DECLSPEC void                redFGetUserHomeDir                      (c
 
 REDGPU_F_DECLSPEC RedFHandleThread *  redFCreateThread                        (uint64_t count);
 REDGPU_F_DECLSPEC void                redFDestroyThread                       (RedFHandleThread * handles);
+REDGPU_F_DECLSPEC void                redFThreadSetProcedure                  (RedFHandleThread handle, void (*procedure)(void));
 REDGPU_F_DECLSPEC RedFBool32          redFThreadIsRunning                     (RedFHandleThread handle);
 REDGPU_F_DECLSPEC void                redFThreadGetName                       (RedFHandleThread handle, char ** outName, uint64_t * outNameBytesCount); // redFFree() outName[0] yourself
 REDGPU_F_DECLSPEC void                redFThreadSetName                       (RedFHandleThread handle, const char * name);
